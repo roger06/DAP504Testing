4e6d714387d2dcc8ccc10ce3a004126ce360b963
maximillion1999 <mr.max.laskey@gmail.com>
2020-06-03 11:19:20 +0100

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+        Player thePlayer = new Player("Donald", "Duck");
+        System.out.println("The Player first name is: "+thePlayer.firstName+". "+"The Player Last Name is: "+thePlayer.lastName+".");
+    }
+}
---
+++ src/com/company/Player.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Player {
+
+    String firstName;
+    String lastName;
+
+    public Player(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
